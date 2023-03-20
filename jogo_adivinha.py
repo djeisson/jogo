89fd03f815b22e9a1c8db9e6739357d44f1f1fbf
Djeisson Schneider <66392853+djeisson@users.noreply.github.com>
2023-03-20 12:17:07 -0300

    upload codigo
    codigo aprimorado para facilitar visualizacao e manutencao

--- jogo_adivinha.py
+++ jogo_adivinha.py
@@ -1,67 +1,66 @@
 import time
 from random import randint
 
+chances = " "
+acertou = bool
 name = " "
+#opcao de salvar
+def salvar():
+    with open('dados.txt', 'a') as arquivo:
+                for valor in name, chances, acertou:
+                    arquivo.write(str(valor) + '         ')
+                    print("feito")
+                arquivo.write("\n")
 
-    #cabeçalho
-def cabecalho():
-    print("*" * 50)
-    print("**             Jogo de Adivinhação              **")
-    print("*" * 50)
-    print("\n" * 3)
-    print("-" * 50)
+#opcoes do jogador
+def opcao():
     print("1 para jogar\n2 para verificar o placar dos lideres\n3 para creditos\n4 sair")
-    print("-" * 50)
-
     botao1 = int(input(""))
     if botao1 == 1:
-        print("\n" * 50)
-        print("carregando!!!")
-        time.sleep(2)
+        carregar()
         jogar()
     elif botao1 == 2:
-        print("\n" * 50)
-        print("carregando!!!")
-        time.sleep(2)
+        carregar()
         historico()
     elif botao1 == 3:
-        print("\n" * 50)
-        print("carregando!!!")
-        time.sleep(2)
+        carregar()
         creditos()
     elif botao1 == 4:
         print("saindo")
         breakpoint
     else:
+        carregar()
         print("voce digitou algo não esperado pelo sistema, tente novamente!")
-        return
+        return opcao()
+
+#funcao de tempo e carregamento    
+def carregar():
+    print("\n" * 50)
+    print("carregando!!!")
+    time.sleep(2)
+    
 
 
-#placar dos lideres
+    #cabeçalho
+
+#cabecalho
+def cabecalho():
+    print("*" * 50)
+    print("**             Jogo de Adivinhação              **")
+    print("*" * 50)
+    print("\n" * 3)
+    
+
+    opcao()
+
+#historico de jogadas
 def historico():
     with open('dados.txt', 'r') as arquivo:
         for valor in arquivo:
             print(valor)
     time.sleep(3)
-    print("-" * 30)
-    opcao2 = int(input("o que deseja fazer agora?\n1 para jogar\n2 para verificar o placar dos lideres\n3 para creditos"))
-    if opcao2 == 1:
-        print("\n" * 50)
-        print("carregando!!!")
-        time.sleep(2)
-        jogar()    
-    elif opcao2 == 2:
-        print("\n" * 50)
-        print("carregando!!!")
-        time.sleep(2)
-        historico()
-    elif opcao2 == 3:
-        print("\n" * 50)
-        print("carregando!!!")
-        time.sleep(2)
-        creditos()
-    else:
-        print()
+    opcao()
+    
 
 def creditos():
     print("escrever bio do desenvolvedor")
@@ -94,7 +93,6 @@ def jogar():
 
     while chances != 3:
         chances = chances + 1
-        print("\n" * 20)
         print(f"vamos lá {name}! \nVocê deve acertar um numero entre 0  e 10")
         chute = int(input(""))
         if chute == numero:
@@ -109,36 +107,13 @@ def jogar():
                 arquivo.write("\n")
                 
             #chamaar sql para salvar dados no bd?
-            time.sleep(5)
-            print("\n" * 50)
-            opcao2 = int(input("O que deseja fazer agora?\n1 para jogar\n2 para verificar o placar dos lideres\n3 para creditos\n4 sair"))
-            if opcao2 == 1:
-                print("\n" * 50)
-                print("carregando!!!")
-                time.sleep(2)
-                jogar()    
-            elif opcao2 == 2:
-                print("\n" * 50)
-                print("carregando!!!")
-                time.sleep(2)
-                historico()
-            elif opcao2 == 3:
-                print("\n" * 50)
-                print("carregando!!!")
-                time.sleep(2)
-                creditos()
-            elif opcao2 == 4:
-                print("saindo")
-                break
-            else:
-                print("")
+            
+            opcao()
             
         elif chute != numero:
             acertou = False
             if chances != 3:
                 print(f"voce errou, esta foi sua {chances}° chance!")
-                time.sleep(3)
-                print("\n" *30)
             elif chances == 3:
                 print(f"voce errou, esta foi sua {chances}° chance!")
                 print(f"o numero era {numero}")
@@ -148,29 +123,7 @@ def jogar():
                         arquivo.write(str(valor) + '         ')
                         print("feito")
                     arquivo.write("\n")
-                time.sleep(5)
-                print("\n" *30)
-                opcao2 = int(input("o que deseja fazer agora?\n1 para jogar\n2 para verificar o placar dos lideres\n3 para creditos\n4 sair"))
-                if opcao2 == 1:
-                    print("\n" * 50)
-                    print("carregando!!!")
-                    time.sleep(2)
-                    jogar()    
-                elif opcao2 == 2:
-                    print("\n" * 50)
-                    print("carregando!!!")
-                    time.sleep(2)
-                    historico()
-                elif opcao2 == 3:
-                    print("\n" * 50)
-                    print("carregando!!!")
-                    time.sleep(2)
-                    creditos()
-                elif opcao2 == 4:
-                    print("saindo")
-                    break
-                else:
-                    print()
+                opcao()
 
 
 cabecalho()
